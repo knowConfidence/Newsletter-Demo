838f276e07063c2895332674cc13ecb3f4762dfd
knowConfidence <gsjheeta55@hotmail.com>
2017-07-07 19:41:18 +0100

    Add files via upload

---
+++ jScript.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+
+	resizeCard();
+
+	$(window).resize(function(){
+
+		resizeCard();
+
+	});
+
+});
+
+function resizeCard(){
+
+	var card = $(".card");
+
+	var i_height = $(".image").outerHeight();
+	var t_height = $(".info").outerHeight();
+	var height = 0;
+
+	if($(".image").position().left == $(".info").position().left){
+
+		height = i_height + t_height;
+	}
+	else if(t_height > i_height){
+
+		height = t_height;
+	}
+	else{
+
+		height = i_height;
+	}
+
+	card.css("height", height);
+}
